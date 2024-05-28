93fdd5a11b617f95d112ad3f019251222509f374
Pankaj Mehta <123pankajmehta@gmail.com>
2024-05-28 22:15:08 +0545

    Made a sample model named Profile.

--- backend/tourRadar/models.py
+++ backend/tourRadar/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    gender = models.CharField(max_length=50,default='')
+    phone = models.IntegerField()
+
+    def __str__(self):
+        return self.user.first_name+" "+self.user.last_name
